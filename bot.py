b7d2106c20cf476efa516e1f26782bd4dff0054f
knyaz <31101995github>
2024-05-31 23:36:57 +0800

    Beta Stable

--- bot.py
+++ bot.py
@@ -1,10 +1,31 @@
-from telegram import Bot
+from aiogram import Bot
 from config import BOT_TOKEN, CHANNEL_ID
 
 bot = Bot(token=BOT_TOKEN)
 
 
 async def createpost(title, per_hour, img_src):
-    message = f"New pool available: {title}\nEarnings per hour: {per_hour}\nImage: {img_src}"
-    await bot.send_message(chat_id=CHANNEL_ID, text=message)
+    profit_str = per_hour.split()[0]
+    profit = float(profit_str)
 
+    bronze_earnings = profit / 10
+    silver_earnings = profit
+    gold_earnings = profit * 100
+    platinum_earnings = profit * 500
+
+    message = (
+        f"*New pool available*: {title}\n\n"
+        f"*Earnings per hour:*\n"
+        f"Platinum: {platinum_earnings} $NOT\n"
+        f"Gold: {gold_earnings} $NOT\n"
+        f"Silver: {silver_earnings} $NOT\n"
+        f"Bronze: {bronze_earnings} $NOT\n\n"
+        f"*Subscribe* @coinpools"
+    )
+
+    await bot.send_message(chat_id=CHANNEL_ID, text=message, parse_mode='Markdown')
+
+
+# Don't forget to close the bot session when done
+async def close_bot():
+    await bot.session.close()
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -7,63 +8,71 @@ from config import URL_SILVER, CHROME_DRIVER_PATH, DB_FILE
 from db import initialize_db, insert_pool, select_pool_by_title
 from bot import createpost
 
+async def fetch_and_process_pools():
+    # Настройка Selenium
+    options = Options()
+    options.add_argument("--headless")  # Запуск в фоновом режиме
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    service = Service(CHROME_DRIVER_PATH)
 
-# Настройка Selenium
-options = Options()
-options.add_argument("--headless")  # Запуск в фоновом режиме
-options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
-service = Service(CHROME_DRIVER_PATH)
+    # Инициализация базы данных
+    conn = initialize_db(DB_FILE)
 
-# Инициализация базы данных
-conn = initialize_db(DB_FILE)
+    # Инициализация браузера
+    driver = webdriver.Chrome(service=service, options=options)
 
-# Инициализация браузера
-driver = webdriver.Chrome(service=service, options=options)
+    while True:
+        # Открытие страницы авторизации
+        driver.get(URL_SILVER)
 
-# Открытие страницы авторизации
-driver.get(URL_SILVER)
+        # Ожидание загрузки страницы
+        time.sleep(5)
 
-# Ожидание загрузки страницы
-time.sleep(5)
+        # Получение HTML-кода страницы
+        html = driver.page_source
 
-# Получение HTML-кода страницы
-html = driver.page_source
+        # Парсинг HTML с помощью BeautifulSoup
+        soup = BeautifulSoup(html, 'html.parser')
 
-# Парсинг HTML с помощью BeautifulSoup
-soup = BeautifulSoup(html, 'html.parser')
+        # Поиск всех элементов пулов
+        pools = soup.find_all('li', class_='_item_kyzjh_6')
 
-# Поиск всех элементов пулов
-pools = soup.find_all('li', class_='_item_kyzjh_6')
+        print(f"Найдено пулов: {len(pools)}")
 
-print(f"Найдено пулов: {len(pools)}")
+        for pool in pools:
+            title_element = pool.find('div', class_='_title_1xl6z_82')
+            per_hour_element = pool.find('span', class_='_perHour_1atj8_36')
+            img_element = pool.find('div', class_='_imageContainer_1atj8_22').find('img')
 
-for pool in pools:
-    title_element = pool.find('div', class_='_title_1xl6z_82')
-    per_hour_element = pool.find('span', class_='_perHour_1atj8_36')
-    img_element = pool.find('div', class_='_imageContainer_1atj8_22').find('img')
+            if title_element and per_hour_element and img_element:
+                title = title_element.text.strip()
+                per_hour = per_hour_element.text.strip()
+                img_src = img_element['src']
 
-    if title_element and per_hour_element and img_element:
-        title = title_element.text.strip()
-        per_hour = per_hour_element.text.strip()
-        img_src = img_element['src']
+                if not select_pool_by_title(conn, title):
+                    pool_data = (title, per_hour, img_src)
+                    insert_pool(conn, pool_data)
+                    await createpost(title, per_hour, img_src)
+                else:
+                    print(f"Pool '{title}' already exists in the database.")
+            else:
+                print("Один из элементов не найден:")
+                print(f"Title Element: {title_element}")
+                print(f"Per Hour Element: {per_hour_element}")
+                print(f"Image Element: {img_element}")
 
-        if not select_pool_by_title(conn, title):
-            pool_data = (title, per_hour, img_src)
-            insert_pool(conn, pool_data)
-            await createpost(title, per_hour, img_src)
-        else:
-            print(f"Pool '{title}' already exists in the database.")
-    else:
-        print("Один из элементов не найден:")
-        print(f"Title Element: {title_element}")
-        print(f"Per Hour Element: {per_hour_element}")
-        print(f"Image Element: {img_element}")
+        # Пауза на 55 секунд
+        await asyncio.sleep(55)
 
-# Закрытие браузера
-driver.quit()
+    # Закрытие браузера
+    driver.quit()
 
-# Закрытие соединения с базой данных
-conn.close()
+    # Закрытие соединения с базой данных
+    conn.close()
 
+async def main():
+    await fetch_and_process_pools()
 
+if __name__ == "__main__":
+    asyncio.run(main())
